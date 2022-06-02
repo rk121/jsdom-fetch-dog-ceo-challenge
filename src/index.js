da9f70a3076467443f5a8ea4d2e264047b2dba2d
rk121 <roshankhadka613@gmail.com>
2022-06-02 14:31:30 +1000

    commit tree 1c9d84d98a81c16c2e30578120e94b4ffac0fb0b

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+const imgURL = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+function fetchRandomDogs() {
+  fetch(imgURL)
+    .then((res) => res.json())
+    .then((json) => {
+      addImg(json.message);
+    });
+}
+
+function fetchAllDogs() {
+  fetch(breedUrl)
+    .then((res) => res.json())
+    .then((json) => {
+      const dogBreeds = Object.keys(json.message);
+      const breedUL = document.querySelector("#dog-breeds");
+      const filterDropDown = document.getElementById("breed-dropdown");
+
+      addBreedToList(dogBreeds, breedUL);
+
+      breedUL.addEventListener("click", (e) => (e.target.style.color = "red"));
+
+      filterDropDown.addEventListener("change", (e) => {
+        const filterBy = e.target.value;
+        filterBreed(dogBreeds, breedUL, filterBy);
+      });
+    });
+}
+
+function addImg(dogs) {
+  const dogBreedsList = document.querySelector("#dog-image-container");
+  dogs.forEach((dog) => {
+    const newImg = document.createElement("img");
+    newImg.src = dog;
+    dogBreedsList.append(newImg);
+  });
+}
+
+function addBreedToList(breedList, breedUL) {
+  breedList.forEach((breed) => {
+    const newLI = document.createElement("li");
+    newLI.append(breed);
+    breedUL.append(newLI);
+  });
+}
+
+function filterBreed(breeds, breedUL, filterBy) {
+  breedUL.innerHTML = "";
+  const newBreedList = breeds.filter((breed) => breed.startsWith(filterBy));
+  addBreedToList(newBreedList, breedUL);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchRandomDogs();
+  fetchAllDogs();
+});
